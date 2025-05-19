e9a49f7d832187e28ff616cab361d48659614d7e
HMSatishVishwakarma <satish.vishwakarma@happiestminds.com>
2025-05-19 14:10:09 +0530

    added new method

--- puppy.go
+++ puppy.go
@@ -7,3 +7,11 @@ func Bark() string {
 func Barks() string {
 	return "Woof! Woof!"
 }
+
+func Barking() string {
+	return "Woof! Woof! Woof!"
+}
+
+func BarkingLoud() string {
+	return "WOOF! WOOF! WOOF!"
+}
