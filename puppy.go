5a2e23052197a9a298c727448f1ac34c0c1f8ca0
HMSatishVishwakarma <satish.vishwakarma@happiestminds.com>
2025-05-21 14:16:21 +0530

    new method added

--- puppy.go
+++ puppy.go
@@ -18,6 +18,12 @@ func Barking() string {
 
 func BarkingLoud(s string) string {
 	//
+	// This is a placeholder for the actual implementation1
+	return dog.WhenGrownUp(s)
+}
+
+func BarkingLoud2(s string) string {
+	//
 	// This is a placeholder for the actual implementation
 	return dog.WhenGrownUp(s)
 }
